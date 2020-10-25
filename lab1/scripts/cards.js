275b432e4dd9391458ece4c195674394096745b1
vlad-gogov <vlad-gogov@users.noreply.github.com>
2020-10-25 10:08:18 +0300

    fix

--- lab1/scripts/cards.js
+++ lab1/scripts/cards.js
@@ -1,18 +1,18 @@
 let arrayСards = [
-    {id: 1, src: "image/6.png", importance: "6"},
-    {id: 2, src: "image/7.png", importance: "7"},
-    {id: 3, src: "image/8.png", importance: "8"},
-    {id: 4, src: "image/9.png", importance: "9"},
-    {id: 5, src: "image/10.png", importance: "10"},
-    {id: 6, src: "image/J.png", importance: "Валет"},
-    {id: 7, src: "image/Q.png", importance: "Королева"},
-    {id: 8, src: "image/K.png", importance: "Король"},
-    {id: 9, src: "image/A.png", importance: "Туз"},
+    {id: 1, src: "images/6.png", importance: "6"},
+    {id: 2, src: "images/7.png", importance: "7"},
+    {id: 3, src: "images/8.png", importance: "8"},
+    {id: 4, src: "images/9.png", importance: "9"},
+    {id: 5, src: "images/10.png", importance: "10"},
+    {id: 6, src: "images/J.png", importance: "Валет"},
+    {id: 7, src: "images/Q.png", importance: "Королева"},
+    {id: 8, src: "images/K.png", importance: "Король"},
+    {id: 9, src: "images/A.png", importance: "Туз"},
 ]
 
 function flipCards() {
     let flag = arrayСards.length - 1;
-    if (flag == 0) {
+    if (flag === 0) {
         document.getElementById('back-card').hidden = true;
     }
     let number = Math.floor(Math.random() * arrayСards.length);
@@ -20,10 +20,9 @@ function flipCards() {
     сard.className = "card";
     сard.src = arrayСards[number].src;
     сard.alt = arrayСards[number].importance;
-    if (flag == 0)
-        document.body.prepend(сard);
+    if (flag)
+        document.getElementById("pack").appendChild(сard);
     else
-        document.body.appendChild(сard);
-        arrayСards.splice(number, 1);
-
+        document.getElementById("pack").prepend(сard);
+    arrayСards.splice(number, 1);
 }
